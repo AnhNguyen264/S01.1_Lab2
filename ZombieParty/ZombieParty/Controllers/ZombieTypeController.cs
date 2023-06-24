c4473d3af58389bd3950cf5b63520c9028a077e7
AnhNguyen <1904518@cegepmontpetit.ca>
2023-06-23 22:33:55 -0400

    FCT classe ZombieType
    Ajout de la classe ZombieType

---
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZombieParty.Controllers
+{
+    public class ZombieTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            this.ViewBag.MaListe = new List<Models.ZombieType>()
+    {
+            new Models.ZombieType(){TypeName= "Virus", Id=1},
+        new Models.ZombieType(){TypeName= "Contact", Id=2}
+    };
+
+            return View();
+        }
+    }
+}
---
+++ ZombieParty/ZombieParty/Models/ZombieType.cs
@@ -0,0 +1,11 @@
+﻿namespace ZombieParty.Models
+{
+    public class ZombieType
+    {
+
+        public int Id;
+
+        public string TypeName;
+        
+    }
+}
